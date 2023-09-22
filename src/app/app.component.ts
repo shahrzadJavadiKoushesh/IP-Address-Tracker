ea32a7d94ee5a706c54640dc6c9579df7fceff26
shahrzad <shahrzadjkoushesh@gmail.com>
2023-09-22 13:09:53 +0330

    add formGroup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, AbstractControl, ValidationErrors, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'IP-Address-Tracker';
+
+  inputForm!: FormGroup
+
+  constructor(private fb: FormBuilder){
+    this.inputForm = this.fb.group({
+        ipAddress: ['', Validators.required]
+    })
+  }
 }
